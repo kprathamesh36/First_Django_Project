62642e004b393c0073c59914eea77ccb88d68a3d
kprathamesh36 <kprathamesh36@gmail.com>
2022-09-03 16:55:39 +0530

    Changes till Models

--- StudyBuddy/base/urls.py
+++ StudyBuddy/base/urls.py
@@ -1,11 +1,12 @@
 # To create URL's for base app so that routing can be easy
 
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns =[
     path('',views.home,name='home'),
-    path('rooms/<str:pk>/',views.rooms,name='rooms'),
+    path('room/<str:pk>/',views.room,name='room'),
+    path('/create-room/', views.create_room,name='create-room'),
 
 
 ]
--- StudyBuddy/base/views.py
+++ StudyBuddy/base/views.py
@@ -1,19 +1,29 @@
+from multiprocessing import context
+from tkinter import NONE
 from django.shortcuts import render
+from .models import Room
 
 
-# Create your views here.
+# Create your views here
 
-rooms = [    {'id':1,'name':'Learn Python'},
-    {'id':2,'name':'Connect with Stars'},
-    {'id':3,'name':'Lose Yourself'},
-    ]
-
-
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn Python'},
+#     {'id': 2, 'name': 'Backend Developer'},
+#     {'id': 3, 'name': 'Frontend Developer'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html',context)
 
-def rooms(request,pk):
-    return render(request, 'base/rooms.html')
+def room(request,pk):
+    room = Room.objects.get(id = pk)
+    context = {'room': room}
+    return render(request, 'base/rooms.html', context)
+
+
+def create_room(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
